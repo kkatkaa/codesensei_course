2c9ff8337fadf4a8dcda455a76c8f0f328c4f1f2
Kasia <katka36@tlen.pl>
2018-10-07 13:20:34 +0200

    Guessing game

---
+++ zjazd_pierwszy/guessing_game.rb
@@ -0,0 +1,20 @@
+#Losowanie
+
+number = rand(100)
+# puts number
+
+puts "Podaj cyfrę od 0 do 99:"
+loop do
+  
+i = gets.to_i
+
+  if i == number
+    puts "Zgadłeś, gratulacje!"
+    break
+  elsif number > i
+    puts  "Mniej"
+  else
+    puts "Więcej"
+  end
+
+end
