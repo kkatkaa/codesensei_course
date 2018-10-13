2bd966ddb42598c1104edbf2507fe5e754ffedac
Kasia <katka36@tlen.pl>
2018-10-13 22:52:37 +0200

    Tree

---
+++ homework/tree.rb
@@ -0,0 +1,106 @@
+#choinka
+
+5.times do
+  print " "
+end
+1.times do
+  print "*"
+end
+5.times do
+  print " "
+end
+puts " "
+
+4.times do
+  print " "
+end
+3.times do
+  print "*"
+end
+4.times do
+  print " "
+end
+puts " "
+
+3.times do
+  print " "
+end
+5.times do
+  print "*"
+end
+3.times do
+  print " "
+end
+puts " "
+
+2.times do
+  print " "
+end
+7.times do
+  print "*"
+end
+2.times do
+  print " "
+end
+puts " "
+
+4.times do
+  print " "
+end
+3.times do
+  print "*"
+end
+4.times do
+  print " "
+end
+puts " "
+
+3.times do
+  print " "
+end
+5.times do
+  print "*"
+end
+3.times do
+  print " "
+end
+puts " "
+
+2.times do
+  print " "
+end
+7.times do
+  print "*"
+end
+2.times do
+  print " "
+end
+puts " "
+
+1.times do
+  print " "
+end
+9.times do
+  print "*"
+end
+1.times do
+  print " "
+end
+puts " "
+
+
+11.times do
+  print "*"
+end
+puts " "
+
+5.times do
+  print " "
+end
+1.times do
+  print "*"
+end
+5.times do
+  print " "
+end
+puts " "
