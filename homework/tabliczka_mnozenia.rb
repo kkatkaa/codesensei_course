b78360abe38502298277cd28a4cd94985d39443f
Kasia <katka36@tlen.pl>
2018-10-15 08:34:15 +0200

    Tabliczka mnozenia

---
+++ homework/tabliczka_mnozenia.rb
@@ -0,0 +1,101 @@
+# Napisz program, który wyświetli tabliczkę mnożenia 10x10.
+
+5.times do
+  print " "
+end
+
+11.times do |i|
+  if i > 0
+    print "%-4d" % i
+  end
+end
+puts " "
+
+5.times do
+  print " "
+end
+
+10.times do
+  print "___ "
+end
+puts " "
+
+print "%-4d" % 1, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (1*i)
+  end
+end
+puts " "
+
+print "%-4d" % 2, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (2*i)
+  end
+end
+puts " "
+
+print "%-4d" % 3, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (3*i)
+  end
+end
+puts " "
+
+print "%-4d" % 4, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (4*i)
+  end
+end
+puts " "
+
+print "%-4d" % 5, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (5*i)
+  end
+end
+puts " "
+
+print "%-4d" % 6, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (6*i)
+  end
+end
+puts " "
+
+print "%-4d" % 7, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (7*i)
+  end
+end
+puts " "
+
+print "%-4d" % 8, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (8*i)
+  end
+end
+puts " "
+
+print "%-4d" % 9, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (9*i)
+  end
+end
+puts " "
+
+print "%-4d" % 10, "|"
+11.times do |i|
+  if i > 0
+    print "%-4d" % (10*i)
+  end
+end
+puts " "
