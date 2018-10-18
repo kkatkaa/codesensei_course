6112c725e341319f3095b3e77e36fbdc1c85edb5
Kasia <katka36@tlen.pl>
2018-10-18 13:46:10 +0200

    Exercise Tabliczka mnożenia.rb modified

--- homework/tabliczka_mnozenia.rb
+++ homework/tabliczka_mnozenia.rb
@@ -20,82 +20,16 @@ end
 end
 puts " "
 
-print "%-4d" % 1, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (1*i)
-  end
-end
-puts " "
+i = 1
+n = 0
 
-print "%-4d" % 2, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (2*i)
+while n < 10
+  n += 1
+  print "%-4d" % n, "|"
+  11.times do |i|
+    if i > 0
+      print "%-4d" % (n*i)
+    end
   end
+  puts " "
 end
-puts " "
-
-print "%-4d" % 3, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (3*i)
-  end
-end
-puts " "
-
-print "%-4d" % 4, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (4*i)
-  end
-end
-puts " "
-
-print "%-4d" % 5, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (5*i)
-  end
-end
-puts " "
-
-print "%-4d" % 6, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (6*i)
-  end
-end
-puts " "
-
-print "%-4d" % 7, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (7*i)
-  end
-end
-puts " "
-
-print "%-4d" % 8, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (8*i)
-  end
-end
-puts " "
-
-print "%-4d" % 9, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (9*i)
-  end
-end
-puts " "
-
-print "%-4d" % 10, "|"
-11.times do |i|
-  if i > 0
-    print "%-4d" % (10*i)
-  end
-end
-puts " "
