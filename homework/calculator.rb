40727af5bc1cbd12a73d78baebb2645588d4856b
Kasia <katka36@tlen.pl>
2018-10-23 08:26:08 +0200

    Calculator exercise

---
+++ homework/calculator.rb
@@ -0,0 +1,40 @@
+# Napisz program będący prostym kalkulatorem.
+
+puts "Choose operation:
+1. Add numbers
+2. Subtract numbers
+3. Multiply numbers
+4. Divide numbers
+5. Quit"
+
+puts "What is your choice?"
+operation = gets.chomp
+
+  if operation == "5"
+    puts "Bye, bye!"
+  else
+    puts "Enter first number:"
+    number1 = gets.to_i
+    puts "Enter second number:"
+    number2 = gets.to_i
+    
+      if operation == "1"
+        result = number1 + number2
+        puts "Your result is: #{result}."
+      elsif operation == "2"
+        result = number1 - number2
+        puts "Your result is: #{result}."
+      elsif
+        operation == "3"
+        result = number1 * number2
+        puts "Your result is: #{result}."
+      elsif
+        operation == "4"
+          while number2 == 0
+            puts "Nie można dzielić przez zero, podaj inną liczbę:"
+            number2 = gets.to_i
+          end
+        result = number1 / number2
+        puts "Your result is: #{result}."
+      end
+    end
