404b031aca6fcc8fec309452bc7312f11757c015
Kasia <katka36@tlen.pl>
2018-10-25 10:17:28 +0200

    Pangram modif.

--- homework/pangram.rb
+++ homework/pangram.rb
@@ -1,8 +1,10 @@
+# Pangram to zdanie, w którym przynajmniej raz pojawia się każda litera alfabetu.
+# Napisz metodę, która sprawdzi czy podany ciąg znaków jest pangramem dla języka
+# polskiego.
 
 def pangram?(string)
   string.include?("ą") && string.include?("ć") && string.include?("ę") && string.include?("ł") && string.include?("ń") && string.include?("ó") && string.include?("ś") && string.include?("ź") && string.include?("ż")
 end
-
 
 puts pangram?('Dość błazeństw, żrą mój pęk luźnych fig') #=> true
 puts pangram?('Losowy ciąg znaków')#=> false
