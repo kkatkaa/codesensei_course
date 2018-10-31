0fb6bf83c0c2ff1fcbb8711dcb7f3ac5536cef64
Kasia <katka36@tlen.pl>
2018-10-31 09:05:19 +0100

    Bloki - cwiczenie

---
+++ zjazd_drugi/bloki.rb
@@ -0,0 +1,7 @@
+# Wygeneruj 100-elementową tablicę losowych liczb z zakresu od 0 do 99. Następnie korzystając z
+# metody Array#group_by , stwórz hash, gdzie kluczem będzie reszta z dzielenia elementu przez
+# 10.
+
+randomness = Array.new(100) { rand(100) }
+wynik = randomness.group_by { |i| i%10 }
+puts wynik
